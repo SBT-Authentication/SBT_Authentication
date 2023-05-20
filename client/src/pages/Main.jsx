92ad91d4e41c1e6f7e25732e60bd16143d4ed21b
swon95 <shimtkddnjs13@gmail.com>
2023-05-21 06:29:09 +0900

    [FIX] 지갑 연결 수정

--- client/src/pages/Main.jsx
+++ client/src/pages/Main.jsx
@@ -1,11 +1,49 @@
+import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 const Main = () => {
     const navigate = useNavigate();
 
-    function Qrclick() {
-        return navigate("/QRcode");
-    }
+    const [userAccount, setUserAccount] = useState({
+        isConnect: "",
+        Account: "",
+    });
+    let walletConnect = async () => {
+        const accounts = await window.ethereum.request({
+            method: "eth_requestAccounts",
+        });
+        if (accounts.length > 0) {
+            localStorage.setItem("isConnected", accounts);
+            setUserAccount({ Account: accounts[0] });
+        }
+        if (accounts.length === undefined) {
+            localStorage.removeItem("isConnected");
+            setUserAccount({ Account: "" });
+            console.log(accounts);
+        }
+        navigate("/QRcode");
+    };
+    const getCurrentWalletConnected = async () => {
+        if (window.ethereum) {
+            try {
+                const addressArray = await window.ethereum.request({
+                    method: "eth_accounts",
+                });
+
+                if (addressArray.length > 0) {
+                    setUserAccount({ Account: addressArray[0] });
+                }
+            } catch (err) {
+                console.error(err);
+            }
+        }
+    };
+
+    useEffect(() => {
+        if (userAccount.Account !== null) {
+            getCurrentWalletConnected();
+        }
+    }, []);
 
     function ReaderClick() {
         return navigate("/QRreader");
@@ -13,7 +51,7 @@ const Main = () => {
 
     return (
         <div>
-            <button onClick={Qrclick}>사용자</button>
+            <button onClick={walletConnect}>사용자</button>
             <button onClick={ReaderClick}>관리자</button>
         </div>
     );
