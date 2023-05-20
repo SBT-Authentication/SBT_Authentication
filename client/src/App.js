242179e4f471f3c7a2715a845dd067f41e4a5970
swon95 <shimtkddnjs13@gmail.com>
2023-05-21 01:06:49 +0900

    [FEAT] 지갑 연결

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [userAccount, setUserAccount] = useState({
+        isConnect: "",
+        Account: "",
+    });
+    let walletConnect = async () => {
+        const accounts = await window.ethereum.request({
+            method: "eth_requestAccounts",
+        });
+        if (accounts.length > 0) {
+            localStorage.setItem("isConnected", accounts);
+            setUserAccount({ Account: accounts[0] });
+        }
+        if (accounts.length === undefined) {
+            localStorage.removeItem("isConnected");
+            setUserAccount({ Account: "" });
+        }
+    };
+    const getCurrentWalletConnected = async () => {
+        if (window.ethereum) {
+            try {
+                console.log("aaaaaaaaaaaaaaa");
+                const addressArray = await window.ethereum.request({
+                    method: "eth_accounts",
+                });
+
+                if (addressArray.length > 0) {
+                    setUserAccount({ Account: addressArray[0] });
+                }
+            } catch (err) {
+                console.error(err);
+            }
+        }
+    };
+
+    useEffect(() => {
+        if (userAccount.Account !== null) {
+            getCurrentWalletConnected();
+        }
+    }, []);
+
+    return <div onClick={walletConnect}>연결</div>;
 }
 
 export default App;
